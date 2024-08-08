6a5fbd763cbad426f9058a5f70f3b359e65a6852
mmadiee <149312891+mmadiee@users.noreply.github.com>
2024-08-08 11:43:57 +0800

    Updated links for footer buttons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons" ;
 export default function Footer() {
     return (
         <footer>
-            <button className="email"><FontAwesomeIcon icon={faEnvelope} /></button>
-            <button className="linked-in"><FontAwesomeIcon icon={faLinkedin} /></button>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=demimanipolo222@gmail.com" target="_blank"><button className="email"><FontAwesomeIcon icon={faEnvelope} /></button></a>
+            <a href="https://www.linkedin.com/in/demi-mae-manipolo/" target="_blank"><button className="linked-in"><FontAwesomeIcon icon={faLinkedin} /></button></a>
         </footer>
           
     );
