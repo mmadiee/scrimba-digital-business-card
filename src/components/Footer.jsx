8654f069eaacb457333dcef2842557144fdeb8b2
mmadiee <149312891+mmadiee@users.noreply.github.com>
2024-08-08 10:28:06 +0800

    added icons for footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons" ;
+
 export default function Footer() {
     return (
         <footer>
-            <button className="email">Email</button>
-            <button className="linked-in">LinkedIn</button>
+            <button className="email"><FontAwesomeIcon icon={faEnvelope} /></button>
+            <button className="linked-in"><FontAwesomeIcon icon={faLinkedin} /></button>
         </footer>
           
     );
