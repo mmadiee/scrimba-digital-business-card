78566d176b9b411b19bf5aea023a89db39794fb4
mmadiee <149312891+mmadiee@users.noreply.github.com>
2024-08-08 09:47:11 +0800

    Updated styling for Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,10 +5,10 @@ export default function Main() {
         <a href="https://github.com/mmadiee" target="_blank"><button className="gitHub">GitHub</button></a>
         <a href="https://mmadiee.github.io/odin-landing-page/" target="_blank"><button className="website">Website</button></a>
       </div>
-      <div>
-        <h4>About</h4>
-        <p>Hi! I&apos;m a web developer based in Manila, PH with a strong background
-            in full-stack development and a passion for 
+      <div className="about">
+        <h4>About me</h4>
+        <p>Hi! I&apos;m a web developer based in Manila, Philippines with a strong background
+            in full-stack web development and a passion for 
             creating efficient, user-friendly web systems. </p>
         <p><strong>Let&apos;s connect!</strong></p>
       </div>
